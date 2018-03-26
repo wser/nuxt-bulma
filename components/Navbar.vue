8568b158280fa180aa030ebd6adc194f78219513
wser <w.web.dev@gmail.com>
2018-03-26 15:45:06 +0200

    initial commit

---
+++ components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">{{pkg.name}}</a>
+        <button class="button navbar-burger" @click="showNav = !showNav">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+      <div class="navbar-menu" :class="{'is-active': showNav}">
+        <div class="navbar-end">
+          <nuxt-link class="navbar-item" to="/">Home</nuxt-link>
+          <nuxt-link class="navbar-item" to="/about">About</nuxt-link>
+          <nuxt-link class="navbar-item" to="/projects">Projects</nuxt-link>
+          <nuxt-link class="navbar-item" to="/contact">Contact</nuxt-link>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import pkg from "../package"
+export default {
+  data() {
+    return {
+      pkg,
+			showNav: false
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hi, I'm Viktor Vidakovic</h1>
+      <h2 class="subtitle">a Software Developer based in Brdovec, Croatia.</h2>
+    </div>
+  </section>
+</template>
